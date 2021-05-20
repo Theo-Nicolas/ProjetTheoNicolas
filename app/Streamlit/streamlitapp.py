5df9581f31680dee4ca1f0f3445afa104e10a814
TheoNcls <74415483+TheoNcls@users.noreply.github.com>
2021-05-20 18:39:18 +0200

    ajout de fonction

--- app/Streamlit/streamlitapp.py
+++ app/Streamlit/streamlitapp.py
@@ -2,18 +2,30 @@ import streamlit as st
 import pymongo
 from pymongo import MongoClient
 import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 client = pymongo.MongoClient('mongodb_project', 27017)
 db = client['mycoin']
 collection = db['coin_tb']
-st.title("""Le site des crypto""")
+st.title("""Calculateur pour les crypto-monnaies""")
 
 def nettoyage(x):
     return(x[2:-2])
 def nettoyagechange(x):
     return(x[2:-3])
-
-
+def nettoyagecomplet(x):
+    res=''
+    t=1
+    for i in range(len(x)):
+            if x[i] =='0' or x[i] =='1'or x[i] =='2'or x[i] =='3'or x[i] =='4'or x[i] =='5'or x[i] =='6'or x[i] =='7'or x[i] =='8'or x[i] =='9'or x[i] =='.':
+                res+=x[i]
+            elif x[i]=='B':
+                t=1000000000
+            elif x[i]=='M':
+                t=1000000
+    res=float(res)*t
+    return res
 cursor = collection.find()
 listcrypto =[]
 PNLTot =0
@@ -40,46 +52,35 @@ else:
     image = 'app/Streamlit/image/updown.png'
 st.image(image, width=None)
 
-z = st.multiselect('Select crypto', listcrypto)
+z = st.multiselect('Choisir les crypto-monnaies', listcrypto)
 dftableau=pd.DataFrame(columns =['Nom', 'Prix', 'Changement sur 24h', 'MarketCap', 'Volume 24h', 'Offre disponible'])
+dftableaufloat=pd.DataFrame(columns =['Nom', 'Prix', 'Changement sur 24h', 'MarketCap', 'Volume 24h', 'Offre disponible'])
 for x in z:
-    nom=collection.find_one({'nom':x})['nom']
-    prix=collection.find_one({'nom':x})['prix']
-    change=collection.find_one({'nom':x})['change']
-    marketcap=collection.find_one({'nom':x})['marketcap']
-    vol24h=collection.find_one({'nom':x})['vol24h']
-    offredispo=collection.find_one({'nom':x})['offredispo']
+    nom=collection.find_one({'nom':x})['nom'][0]
+    prix=collection.find_one({'nom':x})['prix'][0]
+    change=collection.find_one({'nom':x})['change'][0]
+    marketcap=collection.find_one({'nom':x})['marketcap'][0]
+    vol24h=collection.find_one({'nom':x})['vol24h'][0]
+    offredispo=collection.find_one({'nom':x})['offredispo'][0]
     tableau={'Nom':nom, 'Prix':prix, 'Changement sur 24h':change, 'MarketCap':marketcap, 'Volume 24h':vol24h, 'Offre disponible':offredispo }
     dftableau = dftableau.append(tableau, ignore_index=True)
+    tableaufloat={'Nom':nom, 'Prix':float(nettoyagecomplet(prix)), 'Changement sur 24h':float(nettoyagecomplet(change)), 'MarketCap':float(nettoyagecomplet(marketcap)), 'Volume 24h':float(nettoyagecomplet(vol24h)), 'Offre disponible':float(nettoyagecomplet(offredispo)) }
+    dftableaufloat = dftableaufloat.append(tableaufloat, ignore_index=True)
 st.table(dftableau)
     #st.write(collection.find_one({'nom':x}))
 monnaie=[]
 MonnaieTot=0
 beneficeTot=0
 for x in z:
-    monnaie.append([x,st.text_input("Enter Your number of "+str(x)+' : ')])
+    monnaie.append([x,st.text_input("Ecrivez votre nombre de "+str(x)+' : ')])
 for j in monnaie:
     if j[1]!='':
         #chan24h = float(nettoyagechange(collection.find_one({'nom':j[0]})['change']))
         prixactu = collection.find_one({'nom':j[0]})['prix'][0][1:]
         change24 = collection.find_one({'nom':j[0]})['change'][0][1:]
-        res=''
-        for i in range(len(prixactu)):
-            if prixactu[i] ==" ":
-                res+=""
-            else:
-                res+=prixactu[i]
-        prixactu=res
-        res=''
-        for i in range(len(change24)):
-            if change24[i] ==" ":
-                res+=""
-            elif change24[i]=="%":
-                res+=""
-            else:
-                res+=change24[i]
-        change24=res
 
+        prixactu=nettoyagecomplet(prixactu)
+        change24=nettoyagecomplet(change24)
         prix24 = float(prixactu)*float(j[1])
         prixdebut = float(prixactu)*100/(100+float(change24))
         benefice = prix24-(prixdebut*float(j[1]))
@@ -91,3 +92,7 @@ if beneficeTot>0:
     st.write("Au total, vous avez un bénéfice sur 24h de ",beneficeTot,"€")
 else:
     st.write("Au total, vous avez un déficit sur 24h de ",beneficeTot,"€")
+
+selecteur = st.selectbox("Choississez le type de graphique :",['Prix','Changement sur 24h','MarketCap','Volume 24h','Offre disponible'])
+fig = px.bar(dftableaufloat, x="Nom", y=selecteur, barmode="group")
+st.plotly_chart(fig)
